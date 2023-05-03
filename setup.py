2e8a06b52991c1a49985964ade0a0fbd9ad69fe3
Zitronenjoghurt <gersdorfdominik@gmail.com>
2023-05-03 16:15:17 +0200

    restructuring

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+setup(
+    name='lemon-weather-generator',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A tool for randomly generating weather with python. Good for e.g. roleplaying or storywriting.',
+    author='Zitronenjoghurt',
+    license='Apache 2.0',
+    install_requires=[],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest==7.3.1'],
+    test_suite='tests',
+)
---
+++ tests/test_main.py
@@ -0,0 +1,4 @@
+from lemon_weather_generator import foo
+
+def test_foo():
+    assert foo() == "bar"
