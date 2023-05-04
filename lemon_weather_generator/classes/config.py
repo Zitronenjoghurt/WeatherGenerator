9e60688f36b7b685b06d6c69c532a9c769aa733e
Zitronenjoghurt <gersdorfdominik@gmail.com>
2023-05-04 18:50:37 +0200

    add missing temperature_unit to Config class

--- lemon_weather_generator/classes/config.py
+++ lemon_weather_generator/classes/config.py
@@ -1,4 +1,5 @@
 class Config:
-    def __init__(self, decimal_digits: int, effects: dict):
+    def __init__(self, decimal_digits: int, temperature_unit: str, effects: dict):
         self.decimal_digits = decimal_digits
+        self.temperature_unit = temperature_unit
         self.effects = effects
