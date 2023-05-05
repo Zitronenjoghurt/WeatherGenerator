37d79e6fb883479b07a3d2ec3e28caf4f7c17709
Zitronenjoghurt <gersdorfdominik@gmail.com>
2023-05-05 22:55:01 +0200

    add probability curve examples for default configured seasons

--- main.py
+++ main.py
@@ -6,5 +6,8 @@ from matplotlib import pyplot as plt
 
 samples = [randomizeWeather('temperate', 'spring') for i in range(1000000)]
 
+plt.title('Spring')
+plt.xlabel('Temperatures in °C')
+plt.ylabel('Amount of samples')
 plt.hist(samples, bins=50)
 plt.show()
