8b8f60bf5119b81f9f9d60e34b2cce766a211781
Zitronenjoghurt <gersdorfdominik@gmail.com>
2023-05-05 22:37:57 +0200

    add normal distribution to default biome season temperatures

--- lemon_weather_generator/modules/weatherRandomizer.py
+++ lemon_weather_generator/modules/weatherRandomizer.py
@@ -1,9 +1,7 @@
-from lemon_weather_generator.classes.config import Config
 from lemon_weather_generator.classes import Biomes
 
 def randomizeWeather(biomeName: str, seasonName: str):
-    config = Config.get_instance()
     biomes = Biomes.get_instance()
     
     biome = biomes[biomeName]
-    return biome[seasonName].randomTemperature(config.decimal_digits)
+    return biome[seasonName].randomTemperature()
