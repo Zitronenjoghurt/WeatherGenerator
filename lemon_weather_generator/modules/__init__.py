e7a990f64deb9e6dd14d83e01b70162c903d581a
Zitronenjoghurt <gersdorfdominik@gmail.com>
2023-05-07 15:04:05 +0200

    remove weatherRandomizer module

--- lemon_weather_generator/modules/__init__.py
+++
@@ -1,1 +0,0 @@
-from .weatherRandomizer import randomizeWeather
--- lemon_weather_generator/modules/weatherRandomizer.py
+++
@@ -1,7 +0,0 @@
-from lemon_weather_generator.classes import Biomes
-
-def randomizeWeather(biomeName: str, seasonName: str):
-    biomes = Biomes.get_instance()
-    
-    biome = biomes[biomeName]
-    return biome[seasonName].randomTemperature()
